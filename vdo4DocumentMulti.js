fbd29c1e392885213e7c1bcfcdb04c34e9c083ed
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-16 15:06:20 +0530

    4 multi doc

---
+++ vdo4DocumentMulti.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://localhost:27017/list", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => console.log("connection successful")).catch((err) => console.log(err));
+
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+const Playlist = new mongoose.model("Playlist", playlistSchema)
+
+const createDoc = async () => {
+    try {
+        const reactPlaylist = new Playlist({
+            name: "Node",
+            ctype: "backend",
+            videos: 80,
+            author: "Thapa",
+            active: true,
+        })
+        const nodePlaylist = new Playlist({
+            name: "Node",
+            ctype: "backend",
+            videos: 80,
+            author: "Thapa",
+            active: true,
+        })
+        const jsPlaylist = new Playlist({
+            name: "js",
+            ctype: "frontend",
+            videos: 80,
+            author: "Thapa",
+            active: true,
+        })
+        const htmlPlaylist = new Playlist({
+            name: "html",
+            ctype: "frontend",
+            videos: 80,
+            author: "Thapa",
+            active: true,
+        })
+        const cssPlaylist = new Playlist({
+            name: "css",
+            ctype: "frontend",
+            videos: 80,
+            author: "Thapa",
+            active: true,
+        })
+    
+        const result = await Playlist.insertMany([reactPlaylist,nodePlaylist,jsPlaylist,htmlPlaylist,cssPlaylist]);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+createDoc();
